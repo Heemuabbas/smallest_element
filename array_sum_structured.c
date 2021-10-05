cd100d11d45739c689b1c0fb13398b52e7f38aec
Heemuabbas <88588760+Heemuabbas@users.noreply.github.com>
2021-10-05 12:45:05 +0530

    Add files via upload

---
+++ array_sum_structured.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int addElements(int *ptr, int size){
+int i;                                         
+  int total = 0;
+  for(i = 0; i < size; i++){
+    total = total + ptr[i];
+  }
+  return total;
+}
+int main(){
+  int arr[] = {2, 4, 6, 8};
+  int size = sizeof arr / sizeof arr[0];
+  int total = 0;
+  total = addElements(arr, size);
+  printf("The sum of array = %d \n", total);
+  return 0;
+}        
